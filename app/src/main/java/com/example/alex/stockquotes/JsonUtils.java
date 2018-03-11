f7765b6f9bea131a8297b4c51c1e77f7ea23708c
Alexander Austin <austinag@g.cofc.edu>
2018-03-10 19:17:41 -0500

    Various Bug Fixes

--- app/src/main/java/com/example/alex/stockquotes/JsonUtils.java
+++ app/src/main/java/com/example/alex/stockquotes/JsonUtils.java
@@ -22,4 +22,4 @@ public class JsonUtils {
         return quote;
     }
 }
-}
+
--- app/src/main/java/com/example/alex/stockquotes/MainActivity.java
+++ app/src/main/java/com/example/alex/stockquotes/MainActivity.java
@@ -9,13 +9,12 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.view.View.OnKeyListener;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private String symbol, name, lastprice, lasttime, change, range;
-    private TextView symcont = this.findViewById(R.id.symbolcont), namecont = this.findViewById(R.id.namecont),
-            lastpricecont = this.findViewById(R.id.lastpricecont),lasttimecont = this.findViewById(R.id.lastpricecont),
-            changecont = this.findViewById(R.id.changecont), rangecont = this.findViewById(R.id.rangecont);
-    private EditText prompt = this.findViewById(R.id.stock_search);
+    private String sym, name, lastprice, lasttime, change, range;
+    private TextView symcont, namecont, lastpricecont ,lasttimecont, changecont, rangecont;
+    private EditText prompt;
 
 
 
@@ -23,7 +22,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        prompt = this.findViewById(R.id.stock_search);
         prompt.setOnKeyListener(mKeyListener);
+
+        symcont = this.findViewById(R.id.symbolcont);
+        namecont = this.findViewById(R.id.namecont);
+        lastpricecont = this.findViewById(R.id.lastpricecont);
+        lasttimecont = this.findViewById(R.id.lasttimecont);
+        changecont = this.findViewById(R.id.changecont);
+        rangecont = this.findViewById(R.id.rangecont);
+
     }
 
 
@@ -33,6 +42,17 @@ public class MainActivity extends AppCompatActivity {
 
                 getStockJson gsj = new getStockJson();
                 gsj.execute(prompt.getText().toString());
+                try {
+                    gsj.get();
+                }catch (Exception e){
+                    Log.i("Error", e.getMessage());
+                }
+                symcont.setText(sym);
+                namecont.setText(name);
+                lastpricecont.setText(lastprice);
+                lasttimecont.setText(lasttime);
+                changecont.setText(change);
+                rangecont.setText(range);
 
                 return false;
             }
@@ -45,18 +65,16 @@ public class MainActivity extends AppCompatActivity {
             Stock stock = new Stock(symbol[0]);
             try {
                 stock.load();
-                symcont.setText(stock.getSymbol());
-                namecont.setText(stock.getName());
-                lastpricecont.setText(stock.getLastTradePrice());
-                lasttimecont.setText(stock.getLastTradeTime());
-                changecont.setText(stock.getChange());
-                rangecont.setText(stock.getRange());
+                sym = stock.getSymbol();
+                name = stock.getName();
+                lastprice = stock.getLastTradePrice();
+                lasttime = stock.getLastTradeTime();
+                change = stock.getChange();
+                range = stock.getRange();
             }catch(Exception e){
-                prompt.setText("Error Retrieving Symbol");
+                Toast.makeText(getApplicationContext(), "Error gathering data", Toast.LENGTH_SHORT).show();
                 Log.i("Error", e.getMessage());
             }
-            symcont.setText(stock.getSymbol());
-            symcont.setText(stock.getSymbol());
             return stock;
 
         }
